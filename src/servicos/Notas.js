10c1496090e8360c7cf484ff109b1de29ff79e83
Lucas Dircksen Balcevicz <145569604+Lucas-dirbal@users.noreply.github.com>
2025-10-24 16:30:10 +0000

    \z

--- src/servicos/Notas.js
+++ src/servicos/Notas.js
@@ -3,7 +3,7 @@ import { db } from "./SQLite";
 export function criaTabela(){
     db.transection((transection)=> {
         transection.executeSql("CREATE TABLE IF NOT EXISTS"+
-        "Notas "+ "(id INTERGER PRIMARY KEY AUTOINCREMENT, )"
+        "Notas "+ "(id INTERGER PRIMARY KEY AUTOINCREMENT, titulo TEXT, categoria TEXT, texto TEXTO);"
         )
     })
 }
